d5c9e71747a723b32f6a19a91d7e7446c5a1953c
Rushikesh Hulage <hulagerushikesh@gmail.com>
2026-09-04 01:11:40 +0530

    release: v0.2.0

--- src/visiontrack/__init__.py
+++ src/visiontrack/__init__.py
@@ -22,7 +22,7 @@ from .tracking.presets import PRESET_NAMES, preset
 from .tracking.tracker import ByteTracker, TrackObservation
 from .video import VideoSummary, track_video, track_webcam
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     # core tracking
